ba2e676acbb484d5923929e015058a5518999af6
Tim Bailey <bai200@dragonfly-ev.nexus.csiro.au>
2016-12-10 12:17:08 +1100

    Removed test code.

--- pyutils/fileio.py
+++ pyutils/fileio.py
@@ -179,31 +179,3 @@ def find_not_duplicates(dirtree):
 def compare_directories(d1, d2):
     pass
 
-#
-# Tests
-#
-
-def test_create_dirtree(dir):
-    dt = DirectoryTree(dir)
-    print('Number dirs: %d' % dt.dirctr)
-    for k in dt.filedict.keys():
-        fulldir = join_path_list([dt.dirlist[i] for i in k])
-        print('Key:', k, 'Path', fulldir)
-        print('  Files:', dt.filedict[k])
-
-def test_find_duplicates_in_tree(dir):
-    dt = DirectoryTree(dir)
-    dups_name = find_duplicate_attribute(dt, True)
-    dups_size = find_duplicate_attribute(dt, False)
-    print('Names: ', dups_name)
-    print('Sizes: ', dups_size)
-    dups_bin = find_duplicates(dt, dups_name)
-    print('Binaries: ', dups_bin)
-
-if __name__ == '__main__':
-    dir1 = os.getcwd() if len(sys.argv) == 1 else sys.argv[1]
-    flags = 0x02
-    if flags & 0x01:
-        test_create_dirtree(dir1)
-    if flags & 0x02:
-        test_find_duplicates_in_tree(dir1)
